a3e1bc4ba7ecff9c25fd127bef027d4b1c87ba5d
MaisyRoach-Krajewski <maisyroach-krajewski@ec.gc.ca>
2025-01-27 15:07:01 -0500

    Changes to generate sample set of polygons based on UAs instead of by tile. Set to sample 50 harvested polygons and 50 non-harvested (forest) polygons from each.

--- scripts/1_generate_sample_polygons.r
+++ scripts/1_generate_sample_polygons.r
@@ -8,18 +8,17 @@ setwd("C:/harvest_risk")
 # Load helper functions ==================================================================================
 source("scripts/helper_functions.R") 
 
-# Get the codes that correspond with each FMU ============================================================
-tile_codes <- st_read('data/raw/ecoforestry_tile_coverage/Pee_maj.shp')
-tile_codes <- paste(tile_codes$feuillet, collapse = "|")
+# # Get the codes that correspond with each FMU ============================================================
+# tile_codes <- st_read('data/raw/ecoforestry_tile_coverage/Pee_maj.shp')
+# tile_codes <- paste(tile_codes$feuillet, collapse = "|")
 
 
 # Create sample set using custom function 'sample_polygons' ==============================================
-sample.set <- sample_polygons(10, # number of harvested polygons you want
-                              10, #number of non-harvested polygons you want
-                              tile_codes,
-                              shapefile_path = 'data/raw/ecoforestry_4e_shapefiles/')
+sample.set <- sample_by_AU(10, # number of years back tha you want to consider as 'recent' harvest 
+                              50, # number of harvested polygons you want
+                              50, #number of non-harvested polygons you want
+                              shapefile_path = 'data/processed/data_by_UA/')
 
 #Save dataset into processed data folder =================================================================
 st_write(sample.set, 'data/processed/sample_polygons.shp', overwrite = T)
 
-rm(list = ls())
