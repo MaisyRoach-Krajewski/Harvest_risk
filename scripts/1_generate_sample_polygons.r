520420ddb513feaffb72c8f3a9a129db9e7b77d0
MaisyRoach-Krajewski <maisyroach-krajewski@ec.gc.ca>
2025-01-28 09:59:44 -0500

    Minor adjustments

--- scripts/1_generate_sample_polygons.r
+++ scripts/1_generate_sample_polygons.r
@@ -10,9 +10,9 @@ source("scripts/helper_functions.R")
 
 # Create sample set using custom function 'sample_polygons' ==============================================
 sample.set <- sample_by_AU(10, # number of years back tha you want to consider as 'recent' harvest 
-                              50, # number of harvested polygons you want
-                              50, #number of non-harvested polygons you want
-                              shapefile_path = 'data/processed/data_by_UA/')
+                           50, # number of harvested polygons you want
+                           50, #number of non-harvested polygons you want
+                           shapefile_path = 'data/processed/data_by_UA/')
 
 #Save dataset into processed data folder =================================================================
 st_write(sample.set, 'data/processed/sample_polygons.shp', overwrite = T, append = FALSE)
